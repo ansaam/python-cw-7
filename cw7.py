4ec5d337e7f7b58de23aa770790f6ec4dd462c32
ansaam <elaf_ef@hotmail.com>
2023-09-09 23:25:05 +0300

    تم حل الواجب7

---
+++ cw7.py
@@ -0,0 +1,22 @@
+class person:
+    def_init_(self,name,age):
+    self.name = name
+    self.age = age
+
+
+    def is_adult (self):
+        if self.age >18:
+            print("you have too much responsibilities")
+        else:
+            print("Lucky you")
+
+            def_str_(self):
+            return f'my name is{self.name}and I am{self.age} years old.'
+        
+        first_person = person("Ansam", 15)
+        first_person.is_adult()
+        print(first_person)
+ 
+
+    
+    
